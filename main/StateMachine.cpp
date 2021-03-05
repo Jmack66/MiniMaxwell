4d4175e605032086e46c131f9c875b538d86af96
mack.jonah@gmail.com <mack.jonah@gmail.com>
2021-03-05 11:49:59 -0800

    added some more stuff

--- main/StateMachine.cpp
+++ main/StateMachine.cpp
@@ -2,16 +2,46 @@
 
 
 
+StateMachine::StateMachine(){
+
+  Serial.begin(115200); //maybe put other harmless initialization stuff
+  current_ = State::SAFE; // initial state of the system
+  
+}
+
 void StateMachine::update() {
   switch (current_) {
+    case State::ABORT: {
+        //functionality during ABORT mode
+        break;
+      }
     case State::SAFE: {
         //functionality during safe mode
         break;
       }
     case State::IDLING: {
-       //functionality during IDLE 
-       break;
+        //functionality during IDLING
+        break;
       }
-      //TODO: ADD THE OTHER STATES
+    case State::START_UP: {
+        //functionality during IDLE
+        break;
+      }
+    case State::PRESS: {
+        //functionality during PRESS
+        break;
+      }
+    case State::PURGE: {
+        //functionality during PURGE
+        break;
+      }
+    case State::RUNNING: {
+        //functionality during RUNNING
+        break;
+      }
+    case State::SHUT_DOWN: {
+        //functionality during SHUT_DOWN
+        break;
+      }
   }
 }
--- main/StateMachine.h
+++ main/StateMachine.h
@@ -1,13 +1,12 @@
 #pragma once 
 #include "States.h"
-
+#include <Wire.h>
 
 
 class StateMachine{
   public:
+    StateMachine();
     void update();
-
-
   private:
     State current_;
     State previous_;
